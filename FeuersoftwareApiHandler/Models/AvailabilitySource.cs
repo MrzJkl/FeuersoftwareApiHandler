5b20b9353a945ec22a6dbc2be56ecd6f5b259bfa
Julian Stange <stangejm@gmail.com>
2021-02-22 11:49:09 +0100

    Benutzer Verfügbarkeit hinzgefügt

---
+++ FeuersoftwareApiHandler/Models/AvailabilitySource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeuersoftwareApiHandler.Models
+{
+    /// <summary>
+    /// Enum für die Quelle einer Verfügbarkeit
+    /// </summary>
+    public enum AvailabilitySource
+    {
+        none = 0,
+        manual = 1,
+        pager = 2,
+        auto = 3,
+        scheduled = 4,
+        scheduledRecurring = 5,
+    }
+}
+
