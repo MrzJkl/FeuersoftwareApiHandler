0cd316e97bc26bef4183372a7423775c07ba9eb9
Julian Stange <49147127+JulianStange@users.noreply.github.com>
2021-02-22 11:20:37 +0100

    Fahrzeug Status hinzugefügt (#2)
    Co-authored-by: Julian Stange <stangejm@gmail.com>

---
+++ FeuersoftwareApiHandler/Models/VehicleStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FeuersoftwareApiHandler.Models
+{
+    /// <summary>
+    /// Ein Fahrzeug Status
+    /// </summary>
+    public class VehicleStatus
+    {
+        /// <summary>
+        /// Der Status des Fahrzeuges
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Dies Position des Fahrzeuges
+        /// </summary>
+        public Position Position { get; set; }
+
+        /// <summary>
+        /// Der Timestamp des Status
+        /// </summary>
+        public DateTime StatusTimestamp { get; set; }
+
+        /// <summary>
+        /// Der Timestamp der Position
+        /// </summary>
+        public DateTime PositionTimestamp { get; set; }
+    }
+}
--- FeuersoftwareApiHandler/Services/ApiService.cs
+++ FeuersoftwareApiHandler/Services/ApiService.cs
@@ -196,5 +196,89 @@
 
             await client.SendAsync(request);
         }
+
+        /// <summary>
+        /// Sendet einen Fahrzeug Status an Connect mit der Funkgeräte Kennung
+        /// </summary>
+        /// <param name="radioId">Die radioId des Fahrzeuges</param>
+        /// <param name="vehicleStatus">Den Fahrzeugstatus <see cref="VehicleStatus"/></param>
+        /// <returns>Nichts</returns>
+        public async Task PostVehicleStatusByRadioId(string radioId, VehicleStatus vehicleStatus)
+        {
+            if (vehicleStatus == null)
+            {
+                throw new ArgumentNullException(nameof(vehicleStatus));
+            }
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "interfaces/public/vehicle/"+radioId+"/status")
+            {
+                Content = new StringContent(System.Text.Json.JsonSerializer.Serialize(vehicleStatus), Encoding.UTF8, "application/json")
+            };
+
+            await client.SendAsync(request);
+        }
+
+        /// <summary>
+        /// Sendet einen Fahrzeug Status an Connect mit der Id
+        /// </summary>
+        /// <param name="id">Die Connect Id des Fahrzeuges</param>
+        /// <param name="vehicleStatus">Den Fahrzeugstatus <see cref="VehicleStatus"/></param>
+        /// <returns>Nichts</returns>
+        public async Task PostVehicleStatus(int id, VehicleStatus vehicleStatus)
+        {
+            if (vehicleStatus == null)
+            {
+                throw new ArgumentNullException(nameof(vehicleStatus));
+            }
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "interfaces/public/vehicle/" + id + "/status")
+            {
+                Content = new StringContent(System.Text.Json.JsonSerializer.Serialize(vehicleStatus), Encoding.UTF8, "application/json")
+            };
+
+            await client.SendAsync(request);
+        }
+
+        /// <summary>
+        /// Gibt den Fahrzeug Status einer bestimmten Id
+        /// </summary>
+        /// <param name="id">Die Connect Id des Fahrzeuges</param>
+        /// <returns>Fahrzeug Status</returns>
+        public async Task<VehicleStatus> GetVehicleStatusById(int id)
+        {
+            VehicleStatus vehicleStatus = new VehicleStatus();
+            HttpResponseMessage response = await client.GetAsync("interfaces/public/vehicle/"+id+"/status");
+            if (response.IsSuccessStatusCode)
+            {
+                vehicleStatus = await response.Content.ReadAsAsync<VehicleStatus>();
+            }
+            else
+            {
+                throw new HttpRequestException();
+            }
+
+            return vehicleStatus;
+        }
+
+        /// <summary>
+        /// Gibt den Fahrzeug Status einer bestimmten RadioId
+        /// </summary>
+        /// <param name="radioId">Die radioId des Fahrzeuges</param>
+        /// <returns>Fahrzeug Status</returns>
+        public async Task<VehicleStatus> GetVehicleStatusByRadioId(string radioId)
+        {
+            VehicleStatus vehicleStatus = new VehicleStatus();
+            HttpResponseMessage response = await client.GetAsync("interfaces/public/vehicle/" + radioId + "/status");
+            if (response.IsSuccessStatusCode)
+            {
+                vehicleStatus = await response.Content.ReadAsAsync<VehicleStatus>();
+            }
+            else
+            {
+                throw new HttpRequestException();
+            }
+
+            return vehicleStatus;
+        }
     }
 }
